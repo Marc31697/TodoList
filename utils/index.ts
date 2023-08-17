9538c62f54bd12601f70e7a7f416a31540155405
marc31697 <marquese.tech@gmail.com>
2023-08-17 13:37:09 -0400

    Created utils
    Created a utils folder for saving and loading todos from local storage

---
+++ utils/index.ts
@@ -0,0 +1,23 @@
+type Task = {
+  id: string;
+  task: string;
+  completed: boolean;
+  createdAt: Date;
+  completedAt: Date;
+};
+
+export type Tasks = {
+  [category: string]: Task[];
+};
+
+export const loadTasks = () => {
+  if (typeof localStorage === "undefined") return {};
+
+  const tasksJSON = localStorage.getItem("TASKS");
+  if (tasksJSON == null) return {};
+  return JSON.parse(tasksJSON);
+};
+
+export const saveTasks = (tasks: Tasks) => {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+};
