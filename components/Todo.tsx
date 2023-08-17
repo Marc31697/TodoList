d831e07819804edb3590cad8c67d31ec2052fef4
marc31697 <marquese.tech@gmail.com>
2023-08-17 14:03:42 -0400

    Light styling changes

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -5,11 +5,11 @@ interface TodoProps {
 }
 const Todo = ({ todo }: TodoProps) => {
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row justify-between text-xl border-b-[1px] border-gray-900 mb-2">
       <div>{todo}</div>
       <input
         type="checkbox"
-        className="border-none focus:ring-0 bg-gray-800 text-gray-800 rounded-sm"
+        className="border-none focus:ring-0 bg-gray-800 text-gray-800 rounded-sm h-6 w-6"
       />
     </div>
   );
